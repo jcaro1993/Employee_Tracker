2e858dbbeb71f108580acd6c646b52c208fd67b3
Javier Caro <jcaro1993@gmail.com>
2020-12-09 19:40:13 -0800

    adjusting promt and responses

--- server.js
+++ server.js
@@ -24,27 +24,47 @@ connection.connect((err) => {
     start()
 })
 
-function start() {
-    inquirer
+async function start() {
+
+    const results = await inquirer
         .prompt({
             type: 'list',
             message: "what would you like to do ?",
             choices: ["add department", "add role", "add employee", "view departments", "view roles", "view employee", "Update employee roles"],
             name: "choices"
-        }).then((response) => {
-            console.log(response.choices)
-            switch (response.choices) {
-                case "add department":
-                    addDepartment()
-                    break;
-                case "view departments":
-                    viewDepartments()
-                    break;
-                    // ADD THE REST !!!!!!!!!!! ROLE , EMPLOYEE
-            }
-        })
+        });
+    switch (results.choices) {
+        case "add department":
+            addDepartment()
+            break;
+
+        case "view departments":
+            viewDepartments()
+            break;
+
+        case "add role":
+            addRole()
+            break;
+
+        case "view role":
+            viewRole()
+            break;
+
+        case "Update employee roles":
+            updateEmployeerole()
+            break;
+
+        case "add employee":
+            addEmployee()
+            break;
+
+        case "view employee":
+            ViewEmployee()
+            break;
+    }
 }
 
+// department function
 function viewDepartments() {
     connection.query("SELECT * FROM department;", (err, res) => {
         if (err) throw err;
@@ -57,8 +77,8 @@ function addDepartment() {
     inquirer
         .prompt([{
                 type: "number",
-                message: "dwhats department ID",
-                name: "depId"
+                message: "whats department ID",
+                name: "id"
             },
             {
                 type: "input",
@@ -75,6 +95,149 @@ function addDepartment() {
             }
         })
 }
+
+// role function
+
+function viewRole() {
+    connection.query("SELECT * FROM roles;", (err, res) => {
+        if (err) throw err;
+        console.table(res)
+        start()
+    })
+}
+
+function addRole() {
+    inquirer
+        .prompt([{
+                type: "number",
+                message: "whats thier role ID ",
+                name: "id"
+            },
+            {
+                type: "input",
+                message: "what is their title",
+                name: "title"
+            },
+            {
+                type: "number",
+                message: "what is their anuanlly salary",
+                name: "salary"
+            },
+            {
+                type: "number",
+                message: "whats thier department ID ",
+                name: " department_id"
+            }
+        ]).then((answer) => {
+            connection.query("INSERT INTO employee_db.role SET ?;",
+                answer,
+                (err, res) => {
+                    if (err) throw err;
+                    console.log("role added")
+                    start()
+                })
+        })
+}
+
+// employee function
+
+function ViewEmployee() {
+    connection.query("SELECT * FROM employee;", (err, res) => {
+        if (err) throw err;
+        console.table(res)
+        start()
+    })
+}
+
+function addEmployee() {
+    inquirer
+        .prompt([{
+                type: "input",
+                message: "what is Employee first name",
+                name: "first_name"
+            },
+            {
+                type: "input",
+                message: "what is Employee last name",
+                name: "last_name"
+            },
+            {
+                type: "number",
+                message: "what is employees role ID ",
+                name: "role_id"
+            },
+            {
+                type: "number",
+                message: "what is thier managers ID ",
+                name: "manager_id"
+            }
+        ]).then((answer) => {
+            connection.query("INSERT INTO employee_db.employee SET ?;",
+                answer,
+                (err, res) => {
+                    if (err) throw err;
+                    console.log("employee added")
+                    start()
+                })
+        })
+}
+
+// update employee function 
+
+function updateEmployeerole() {
+    connection.query("SELECT * FROM employee", function(err, results) {
+        if (err) throw err;
+
+        inquirer
+            .prompt([{
+                    name: "choice",
+                    type: "rawlist",
+                    choices: function() {
+                        var choiceArray = [];
+                        for (var i = 0; i < results.length; i++) {
+                            choiceArray.push(results[i].first_name);
+                        }
+                        return choiceArray;
+                    },
+                    message: "what employee do you want to update ?"
+                }
+
+            ])
+            .then(function(answer) {
+                console.log(answer.choice)
+                connection.query("SELECT * FROM roles", function(err, results) {
+                    if (err) throw err;
+
+                    inquirer
+                        .prompt([{
+                                name: "choice",
+                                type: "rawlist",
+                                choices: function() {
+                                    var choiceArray = [];
+                                    for (var i = 0; i < results.length; i++) {
+                                        choiceArray.push(results[i].title);
+                                    }
+                                    return choiceArray;
+                                },
+                                message: "what role do you want to update ?"
+                            }
+
+                        ])
+                        .then(function(answer) {
+                            console.log(answer.choice)
+
+
+                        })
+                });
+
+            })
+    });
+}
+
+
+
+
+
 
 // runs node
 // inquired with questions
